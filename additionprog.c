bf9770e9e78cd09562b6890acba10ea3cbe2fa69
Abhighnan Kuruva <68819095+loyalabhi@users.noreply.github.com>
2023-04-17 18:32:02 +0530

    Add files via upload

---
+++ additionprog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main (){
+  
+int number1, number2, sum;
+  
+printf ("Enter two numbers: ");
+  
+scanf ("%d %d", &number1, &number2);
+  
+    // calculate the sum of the two integers
+    sum = number1 + number2;
+  
+printf ("%d + %d = %d", number1, number2, sum);
+  
+return 0;
+
+}
+
+
